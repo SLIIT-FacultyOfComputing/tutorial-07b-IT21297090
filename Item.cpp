56cd5b2830ad975a858fe18cfc91c697bf7db641
Chamodi Maheesha <99491092+IT21297090@users.noreply.github.com>
2022-03-26 20:45:15 +0000

    Item.cpp is completed

--- Item.cpp
+++ Item.cpp
@@ -3,22 +3,35 @@
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
+Item :: Item()
+{
+  itemCode = 0;
+  unitPrice = 0.0;
+}
 // 5. Implement Overloaded Constructor Implementation
+Item :: Item(int iCode , float uPrice)
+{
+  itemCode = iCode;
+  unitPrice = uPrice;
+}
 // 6. Implement Destructor (display "Destructor Called")
+Item :: ~Item()
+{
+  cout << "Destructor Called" << endl;
+}
+
 
 void Item::setDiscount(float pdiscount) {
-  discount = pdiscount;
+discount = pdiscount;
 }
-
 float Item::getDiscount() {
-  return discount;
+return discount;
 }
-
 float Item::discountedPrice() {
-   return unitPrice - unitPrice * discount/100;
+return unitPrice - unitPrice * discount/100;
+}
+void Item::display() {
+cout << "Item : " << itemCode << endl;
+cout << "Discounted Price " << discountedPrice() << endl;
 }
 
-void Item::display() {
-  cout << "Item : " << itemCode << endl;
-  cout << "Discounted Price " << discountedPrice() << endl;
-}
